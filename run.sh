3834b60819726865e29bb8105908d3b13ecf273c
young <young@slc11xdn.us.oracle.com>
2019-03-30 02:36:23 -0700

    dockerized

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-run --name chb_server -e https_proxy=http://www-proxy-brmdc.us.oracle.com:80 -e http_proxy=http://www-proxy-brmdc.us.oracle.com:80 -e action_server=http://action_server:9999 -p 8080:18888 -d chb_server:0.1
+docker run --name chb_server -e https_proxy=http://www-proxy-brmdc.us.oracle.com:80 -e http_proxy=http://www-proxy-brmdc.us.oracle.com:80 -e action_server=http://action_server:9999 -p 8080:18888 -d chb_server:0.1
 
