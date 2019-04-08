84ede44abe07822e97c65153ad706fbe20b01261
young <yon@gmail.com>
2019-04-08 11:29:21 +0000

    add NO_PROXY for node request module

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker run --name chb_server -e https_proxy=http://www-proxy-brmdc.us.oracle.com:80 -e http_proxy=http://www-proxy-brmdc.us.oracle.com:80 -e no_proxy=action_server  -e ACTION_SERVER=http://action_server:19999 -p 8080:18888 --net mynet -d chb:0.1
+docker run --name chb_server -e https_proxy=http://www-proxy-brmdc.us.oracle.com:80 -e http_proxy=http://www-proxy-brmdc.us.oracle.com:80 -e NO_PROXY=action_server -e no_proxy=action_server  -e ACTION_SERVER=http://action_server:19999 -p 8080:18888 --net mynet -d chb:0.1
 
